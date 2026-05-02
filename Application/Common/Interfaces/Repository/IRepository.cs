c4138365730f1545d78042d065d03fb47afb172c
Ivan Goncharuk <tgoncharukivan@gmail.com>
2026-05-02 18:54:07 +0300

    feat: Добавить стандартную реализацию repository

---
+++ Application/Common/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces.Repository;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
+}
---
+++ Infrastructure/Database/Repositories/EfRepository.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Interfaces.Repository;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database.Repositories;
+
+public class EfRepository<T> : IRepository<T> where T: BaseEntity
+{
+    protected readonly AppDbContext DbContext;
+    protected readonly DbSet<T> DbSet;
+
+    public EfRepository(AppDbContext dbContext)
+    {
+        DbContext = dbContext;
+        DbSet = DbContext.Set<T>();
+    }
+    
+    public virtual async Task<T?> GetByIdAsync(Guid id)
+        => await DbSet.FindAsync(id);
+
+    public virtual async Task AddAsync(T entity)
+        => await DbSet.AddAsync(entity);
+}
